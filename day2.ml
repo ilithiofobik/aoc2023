dfd10333410a213630d5d6b928e70d8e0f0efc5c
Wojciech Sęk <wojciech.sek17@gmail.com>
2023-12-02 06:47:12 +0100

    added day2

---
+++ day2.ml
@@ -0,0 +1,64 @@
+let list_to_pair l =
+  let num = List.hd l |> int_of_string in
+  let color = List.nth l 1 in 
+  (num, color);;
+
+let line_to_pairs line =
+  line 
+  |> String.split_on_char ':' 
+  |> List.rev 
+  |> List.hd 
+  |> String.map (fun c -> if c = ';' then ',' else c)
+  |> String.split_on_char ',' 
+  |> List.map String.trim 
+  |> List.map (String.split_on_char ' ') 
+  |> List.map list_to_pair;;
+
+let legal_color num color =
+  let max_num =
+    match color with 
+    | "red" -> 12 
+    | "green" -> 13
+    | "blue" -> 14
+    | _ -> failwith "Unknown color" in
+  num <= max_num;;
+
+let legal_line line =
+  line 
+  |> line_to_pairs
+  |> List.for_all (fun (num, color) -> legal_color num color);;
+
+let line_to_power pairs = 
+  let rec aux red blue green ps =
+    match ps with
+    | [] -> red * blue * green
+    | (num, color) :: ps' ->
+      match color with
+      | "red" -> aux (max red num) blue green ps'
+      | "blue" -> aux red (max blue num) green ps'
+      | "green" -> aux red blue (max green num) ps'
+      | _ -> failwith "Unknown color" in
+  aux 0 0 0 pairs;;
+
+let task1 lines =
+  lines
+  |> List.filter (fun (_, line) -> legal_line line)
+  |> List.map fst 
+  |> Utils.list_sum;;
+
+let task2 lines =
+  lines
+  |> List.map snd
+  |> List.map line_to_pairs
+  |> List.map line_to_power
+  |> Utils.list_sum;;
+
+let lines =
+  Utils.file_to_lines ("input/day2.txt") 
+  |> List.mapi (fun i line -> (i + 1, line)) in
+let result1 = task1 lines in
+let result2 = task2 lines in
+  Printf.printf "Task1: %d\n" result1;
+  Printf.printf "Task2: %d\n" result2;
+  assert (result1 = 2149);
+  assert (result2 = 71274);
